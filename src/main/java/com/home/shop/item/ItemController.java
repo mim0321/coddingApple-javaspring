9793495b8571dc3b171051204d6714f5f109749f
dongmin <letong1543@kakao.com>
2024-11-17 02:08:10 +0900

    feat: fetch 활용한 삭제기능

--- src/main/java/com/home/shop/item/ItemController.java
+++ src/main/java/com/home/shop/item/ItemController.java
@@ -3,9 +3,12 @@ package com.home.shop.item;
 
 import com.home.shop.ListService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -17,42 +20,43 @@ public class ItemController {
     private final ListService listService;
 
     @GetMapping("/list")
-    String list(Model model){
+    String list(Model model) {
         listService.listDB(model);
 
         return "list.html";
     }
 
-    /** 상품 추가기능 만들기
+    /**
+     * 상품 추가기능 만들기
      * 1. 상품이름, 가격을 작성할 수 있는 페이지와 폼
      * 2. 전송을 누르면 서버에서 검사(예외처리)
      * 3. 문제 없을 시 DB에 저장하기
-     * */
+     */
 
     @GetMapping("/write")
-    String write(){
+    String write() {
         return "write.html";
     }
 
-// 상품 추가 POST API
-@PostMapping("/add")
+    // 상품 추가 POST API
+    @PostMapping("/add")
     public String addItem(@RequestParam String title,
                           @RequestParam Integer price,
-                          Model model){
+                          Model model) {
 //        Service로 저장하는거 만듦
         itemService.saveItem(title, price);
 
         return "redirect:/list";
     }
 
-//    상세페이지 GET API
+    //    상세페이지 GET API
 //    URL parameters == url에 중괄호 + 작명 >> /{name}
     @GetMapping("/detail/{id}")
-    String detail(@PathVariable Long id, Model model){
+    String detail(@PathVariable Long id, Model model) {
 
         Optional<Item> result = itemRepository.findById(id);
 //        Optional 타입을 사용 할 때에는 findById의 결과가 null이 될 수 있기 때문에 if문을 사용하여 "해당 변수에 뭔가가 들어있으면 result.get()을 실행해줘" 라고 코딩하는 것이 옳다.
-        if (result.isPresent()){
+        if (result.isPresent()) {
             model.addAttribute("item", result.get());
             return "detail.html";
         } else {
@@ -62,19 +66,20 @@ public class ItemController {
     }
 
 // 상품수정 API
+
     /**
      * 1. 수정버튼을 누르면
      * 2. 수정페이지로 이동하고(페이지엔 기존내용이 채워져있음)
      * 3. 전송 누르면 DB로 수정하면 됨
-     * */
+     */
     @GetMapping("/edit/{id}")
-    String postEdit(@PathVariable Long id, Model model){
+    String postEdit(@PathVariable Long id, Model model) {
         Optional<Item> result = itemRepository.findById(id);
 
-        if (result.isPresent()){
+        if (result.isPresent()) {
             model.addAttribute("item", result.get());
             return "edit.html";
-        } else{
+        } else {
             return "redirect:/list";
         }
 
@@ -87,7 +92,7 @@ public class ItemController {
 
         Optional<Item> result = itemRepository.findById(id);
 
-        if(result.isPresent()){
+        if (result.isPresent()) {
             Item updatedItem = result.get();
             updatedItem.setTitle(title);
             updatedItem.setPrice(price);
@@ -97,6 +102,19 @@ public class ItemController {
         return "redirect:/list";
     }
 
+
+    //    삭제 API
+    @DeleteMapping("/deleteItem")
+    ResponseEntity<String> deleteItem(@RequestParam Long id) {
+        Optional<Item> result = itemRepository.findById(id);
+
+        if (result.isPresent()) {
+            itemRepository.deleteById(id);
+            return ResponseEntity.status(200).body("delete complete");
+        } else {
+            return ResponseEntity.status(400).body("400 error");
+        }
+    }
 
 
 }
