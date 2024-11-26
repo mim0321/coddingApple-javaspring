e8a2388c6ac74b0595a83e0f75005651281ef4da
dongmin <letong1543@kakao.com>
2024-11-27 03:35:30 +0900

    feat: 검색기능 제작 중

--- src/main/java/com/home/shop/item/ItemController.java
+++ src/main/java/com/home/shop/item/ItemController.java
@@ -161,6 +161,28 @@ public class ItemController {
         }
     }
 
+//    검색 API
+    @PostMapping("/search/page/{page}")
+    String postSearch(@RequestParam String searchText,
+                      @PathVariable Integer page,
+                      Model model) {
 
+        Page<Item> resultPage = itemRepository.searchWithPagination(searchText, PageRequest.of(page - 1, 5));
+
+        List<Item> items = resultPage.getContent();
+        int totalPages = resultPage.getTotalPages();
+
+        List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                .boxed()
+                .collect(Collectors.toUnmodifiableList());
+
+
+        model.addAttribute("items", items);
+        model.addAttribute("pages", pageNumbers);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("searchText", searchText);
+
+        return "search.html";
+    }
 
 }
